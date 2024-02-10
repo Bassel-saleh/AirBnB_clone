d748d829c5d446c4d1a0a06d84ebe083c78b887f
Bassel-saleh <basselh26@gmai.com>
2024-02-10 14:16:13 +0200

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         '''return representation of an object'''
