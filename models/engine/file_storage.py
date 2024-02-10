177c015ed8dd7f42cbe8895c7765052499aa98de
Bassel-saleh <basselh26@gmai.com>
2024-02-10 17:35:38 +0200

    debugging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,7 @@ class FileStorage:
 
     def all(self):
         """ Returns the dictionary objects """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id
@@ -38,24 +38,24 @@ class FileStorage:
             obj(object) -> The object to add to __objects
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ serializes objects to the JSON file """
         serialized_objects = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
+        with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 loaded_objects = json.load(file)
                 for key, value in loaded_objects.items():
                     class_name, obj_id = key.split('.')
                     cls = self.classes[class_name]
                     self.__objects[key] = cls(**value)
         except FileNotFoundError:
-            pass
+            return
